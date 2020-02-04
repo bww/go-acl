977c5a6deb52416418e0b9a2769d1d8ac7fe107d
Brian W. Wolter <brian@brianwolter.com>
2020-02-04 14:41:04 -0500

    ...

--- etc/db/001_base.up.sql
+++ etc/db/001_base.up.sql
@@ -14,3 +14,9 @@ create table acl_policy (
   data        jsonb                     not null,
   created_at  timestamp with time zone  not null default now()
 );
+
+create table acl_authorization_r_policy (
+  auth_id         uuid                      not null references acl_authorization (id),
+  policy_id       uuid                      not null references acl_policy (id),
+  primary key (auth_id, policy_id)
+);
