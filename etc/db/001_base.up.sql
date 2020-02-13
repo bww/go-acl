2b78a3df015b61f4ba2ed51f580355552df927cd
Brian W. Wolter <brian@brianwolter.com>
2020-02-13 12:48:44 -0500

    Formatting

--- etc/db/001_base.up.sql
+++ etc/db/001_base.up.sql
@@ -1,18 +1,18 @@
 
 create table acl_authorization (
-  id          uuid                      primary key,
-  key         varchar(64)               unique not null,
-  secret      varchar(64)               not null,
-  description varchar(1024),
-  active      boolean                   not null default false,
-  created_at  timestamp with time zone  not null default now()
+  id              uuid                      primary key,
+  key             varchar(64)               unique not null,
+  secret          varchar(64)               not null,
+  description     varchar(1024),
+  active          boolean                   not null default false,
+  created_at      timestamp with time zone  not null default now()
 );
 
 create table acl_policy (
-  id          uuid                      primary key,
-  type        varchar(32)               not null,
-  data        jsonb                     not null,
-  created_at  timestamp with time zone  not null default now()
+  id              uuid                      primary key,
+  type            varchar(32)               not null,
+  data            jsonb                     not null,
+  created_at      timestamp with time zone  not null default now()
 );
 
 create table acl_authorization_r_policy (
